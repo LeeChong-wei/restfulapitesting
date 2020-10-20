2e91739c5baa16a425db637778b1576998342756
chenyang <990776253@qq.com>
2020-10-20 18:09:59 +0800

    display模块优化

--- resource/graph.js
+++ resource/graph.js
@@ -2,16 +2,6 @@ function load_data(){return {
     "links": [
         {
             "source": 0,
-            "target": 1,
-            "type": ""
-        },
-        {
-            "source": 0,
-            "target": 2,
-            "type": ""
-        },
-        {
-            "source": 0,
             "target": 3,
             "type": ""
         },
@@ -21,13 +11,13 @@ function load_data(){return {
             "type": ""
         },
         {
-            "source": 1,
-            "target": 0,
+            "source": 0,
+            "target": 8,
             "type": ""
         },
         {
-            "source": 1,
-            "target": 2,
+            "source": 0,
+            "target": 9,
             "type": ""
         },
         {
@@ -38,6 +28,16 @@ function load_data(){return {
         {
             "source": 1,
             "target": 4,
+            "type": ""
+        },
+        {
+            "source": 1,
+            "target": 8,
+            "type": ""
+        },
+        {
+            "source": 1,
+            "target": 9,
             "type": ""
         },
         {
@@ -46,13 +46,218 @@ function load_data(){return {
             "type": ""
         },
         {
+            "source": 2,
+            "target": 3,
+            "type": ""
+        },
+        {
+            "source": 2,
+            "target": 4,
+            "type": ""
+        },
+        {
+            "source": 2,
+            "target": 8,
+            "type": ""
+        },
+        {
+            "source": 2,
+            "target": 9,
+            "type": ""
+        },
+        {
+            "source": 3,
+            "target": 0,
+            "type": ""
+        },
+        {
             "source": 3,
             "target": 1,
             "type": ""
         },
         {
+            "source": 3,
+            "target": 2,
+            "type": ""
+        },
+        {
+            "source": 3,
+            "target": 4,
+            "type": ""
+        },
+        {
+            "source": 3,
+            "target": 8,
+            "type": ""
+        },
+        {
+            "source": 3,
+            "target": 9,
+            "type": ""
+        },
+        {
+            "source": 4,
+            "target": 0,
+            "type": ""
+        },
+        {
             "source": 4,
             "target": 1,
+            "type": ""
+        },
+        {
+            "source": 4,
+            "target": 2,
+            "type": ""
+        },
+        {
+            "source": 4,
+            "target": 3,
+            "type": ""
+        },
+        {
+            "source": 4,
+            "target": 8,
+            "type": ""
+        },
+        {
+            "source": 4,
+            "target": 9,
+            "type": ""
+        },
+        {
+            "source": 5,
+            "target": 3,
+            "type": ""
+        },
+        {
+            "source": 5,
+            "target": 4,
+            "type": ""
+        },
+        {
+            "source": 5,
+            "target": 8,
+            "type": ""
+        },
+        {
+            "source": 5,
+            "target": 9,
+            "type": ""
+        },
+        {
+            "source": 6,
+            "target": 1,
+            "type": ""
+        },
+        {
+            "source": 6,
+            "target": 3,
+            "type": ""
+        },
+        {
+            "source": 6,
+            "target": 4,
+            "type": ""
+        },
+        {
+            "source": 6,
+            "target": 8,
+            "type": ""
+        },
+        {
+            "source": 6,
+            "target": 9,
+            "type": ""
+        },
+        {
+            "source": 7,
+            "target": 1,
+            "type": ""
+        },
+        {
+            "source": 7,
+            "target": 2,
+            "type": ""
+        },
+        {
+            "source": 7,
+            "target": 3,
+            "type": ""
+        },
+        {
+            "source": 7,
+            "target": 4,
+            "type": ""
+        },
+        {
+            "source": 7,
+            "target": 8,
+            "type": ""
+        },
+        {
+            "source": 7,
+            "target": 9,
+            "type": ""
+        },
+        {
+            "source": 8,
+            "target": 0,
+            "type": ""
+        },
+        {
+            "source": 8,
+            "target": 1,
+            "type": ""
+        },
+        {
+            "source": 8,
+            "target": 2,
+            "type": ""
+        },
+        {
+            "source": 8,
+            "target": 3,
+            "type": ""
+        },
+        {
+            "source": 8,
+            "target": 4,
+            "type": ""
+        },
+        {
+            "source": 8,
+            "target": 9,
+            "type": ""
+        },
+        {
+            "source": 9,
+            "target": 0,
+            "type": ""
+        },
+        {
+            "source": 9,
+            "target": 1,
+            "type": ""
+        },
+        {
+            "source": 9,
+            "target": 2,
+            "type": ""
+        },
+        {
+            "source": 9,
+            "target": 3,
+            "type": ""
+        },
+        {
+            "source": 9,
+            "target": 4,
+            "type": ""
+        },
+        {
+            "source": 9,
+            "target": 8,
             "type": ""
         }
     ],
@@ -61,31 +266,61 @@ function load_data(){return {
             "id": 0,
             "label": "API0",
             "name": "GitLab",
-            "weight": 1
+            "weight": 4
         },
         {
             "id": 1,
             "label": "API1",
             "name": "GitLab",
-            "weight": 4
+            "weight": 7
         },
         {
             "id": 2,
             "label": "API2",
             "name": "GitLab",
-            "weight": 2
+            "weight": 5
         },
         {
             "id": 3,
             "label": "API3",
             "name": "GitLab",
-            "weight": 2
+            "weight": 9
         },
         {
             "id": 4,
             "label": "API4",
             "name": "GitLab",
-            "weight": 2
+            "weight": 9
+        },
+        {
+            "id": 5,
+            "label": "API5",
+            "name": "GitLab",
+            "weight": 0
+        },
+        {
+            "id": 6,
+            "label": "API6",
+            "name": "GitLab",
+            "weight": 0
+        },
+        {
+            "id": 7,
+            "label": "API7",
+            "name": "GitLab",
+            "weight": 0
+        },
+        {
+            "id": 8,
+            "label": "API8",
+            "name": "GitLab",
+            "weight": 9
+        },
+        {
+            "id": 9,
+            "label": "API9",
+            "name": "GitLab",
+            "weight": 9
         }
     ]
 }}
