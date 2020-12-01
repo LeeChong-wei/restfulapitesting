0a45c9ba9d3bb6afd05b341c62cb7c39081faac6
litianyu <3171261794@qq.com>
2020-12-02 01:28:42 +0800

    lty

--- module/traversal.py
+++ module/traversal.py
@@ -5,8 +5,7 @@ import os.path
 my_path = os.path.abspath(os.path.dirname(__file__))
 api_info_list = parse(os.path.join(my_path, "../openapi/project.yaml"), 1.0)
 matrix, weight_info_list = get_dep_info(api_info_list)
-graph = matrix
-
+graph = matrix.tolist()
 # 记录拓扑排序顺序
 topology_order = []
 # 记录出度为0的点
@@ -15,23 +14,24 @@ out_degree_zero = []
 visited = []
 # 设计出度为0的点
 end = []
-for i in range(1,len(graph)):
-    end.append(0)
-
-# 收集出度为0的点的集合
-for i in range(1,len(graph)):
-    if graph[i].__eq__(end):
-        out_degree_zero.append(i)
+for i in range(0, len(graph)):
+    end.append(-1)
+# 收集出度为0的点的集合,即无依赖节点的集合
+for j in range(0, len(graph)):
+    if graph[j] == end:
+        out_degree_zero.append(j)
 
 # 拓扑，通过回溯insert到l中
 def topology_visit(g, n):
     if n not in visited:
         visited.append(n)
-    for m in range(1,len(g)):
-        if g[m][n] and m not in visited:
+    for m in range(0, len(g)):
+        if graph[m][n] != -1 and m not in visited:
             topology_visit(g, m)
-    topology_order.insert(0,n)
+    topology_order.insert(0, n)
 
 
 for n in out_degree_zero:
     topology_visit(graph, n)
+
+print(topology_order)
