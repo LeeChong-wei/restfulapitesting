faa3fa3fc4d9e2deb7b025ece95b3f640f6beccd
linjiaxian <ljx425115084@gmail.com>
2020-12-21 14:27:59 +0800

    add type

--- entity/field_info.py
+++ entity/field_info.py
@@ -1,6 +1,7 @@
 class field_info:
-    def __init__(self,field_name,require,default,fuzz,location):
+    def __init__(self,field_name,type_,require,default,fuzz,location):
         self.field_name = field_name
+        self.field_type = type_
         self.require = require
         self.default = default
         self.fuzz = fuzz
